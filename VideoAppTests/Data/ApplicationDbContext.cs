4531ce00c89a9aa4840c21f2a101d464aa3cf1f0
Franco Neil Glovasa <117660459+francoglov@users.noreply.github.com>
2024-10-17 00:45:51 +0800

    Add project files.

---
+++ VideoAppTests/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using VideoAppTests.Models;
+
+namespace VideoAppTests.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Video>().ToTable("Videos");
+            modelBuilder.Entity<Category>().ToTable("Categories");
+
+            // Ensure Category.Name is unique
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            // Additional configurations can go here
+        }
+    }
+
+}
---
+++ VideoAppTests/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace VideoAppTests.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
